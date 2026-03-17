f59a6b05c9a1d687c1d6e679c4b39b678557168e
Karthic-N-A <nakarthic3@gmail.com>
2026-03-17 19:36:52 +0530

    Test Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart'; 
+import 'firebase_options.dart';
 import 'views/landing_page.dart'; // Importing your new UI file
 
 void main() async {
   // Ensure the Flutter environment is ready before doing anything else
   WidgetsFlutterBinding.ensureInitialized();
-
+  //Testing
   // Initialize Firebase using the auto-generated config from FlutterFire CLI
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const OdoGoApp());
 }
@@ -24,7 +22,7 @@ class OdoGoApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'OdoGo',
       // This tells the app to load the LandingPage first
-      home: LandingPage(), 
+      home: LandingPage(),
     );
   }
 }
